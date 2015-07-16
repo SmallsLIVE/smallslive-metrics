17094cf890447f61f5eebd24c89fcb0c1f8f27d9
Filip Jukic <filip@jukic.me>
2015-07-17 00:01:22 +0200

    settings module

---
+++ metrics/settings/local_filip.py
@@ -0,0 +1,21 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'smallslive',
+        'USER': 'bezidejni',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    },
+
+    'metrics': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'smallslive-metrics',
+        'USER': 'bezidejni',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
