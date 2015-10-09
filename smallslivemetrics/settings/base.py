90e92c34f602c273e9fbae00b0af87da658f76fd
Filip Jukić <filip@jukic.me>
2015-10-09 20:40:50 +0200

    add redis caching

--- smallslivemetrics/settings/base.py
+++ smallslivemetrics/settings/base.py
@@ -27,6 +27,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+def get_env_variable(var_name):
+    """ Get the environment variable or return exception """
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
 
 # Application definition
 
--- smallslivemetrics/settings/heroku.py
+++ smallslivemetrics/settings/heroku.py
@@ -1,3 +1,4 @@
+import urlparse
 import dj_database_url
 from .base import *
 
@@ -35,3 +36,36 @@ CORS_ALLOW_METHODS = (
     'POST',
     'OPTIONS'
 )
+
+# Cache
+redis_url = urlparse.urlparse(get_env_variable('REDIS_URL'))
+CACHEOPS_REDIS = {
+    'host': redis_url.hostname ,
+    'port': redis_url.port,
+    'db': 1,
+    'password': redis_url.password,
+    'socket_timeout': 5,   # connection timeout in seconds, optional
+}
+
+CACHEOPS = {
+    # Automatically cache any User.objects.get() calls for 15 minutes
+    # This includes request.user or post.author access,
+    # where Post.author is a foreign key to auth.User
+    'auth.user': {'ops': 'get', 'timeout': 60*15},
+
+    # Automatically cache all gets and queryset fetches
+    # to other django.contrib.auth models for an hour
+    'auth.*': {'ops': ('fetch', 'get'), 'timeout': 60*60},
+
+    # Cache gets, fetches, counts and exists to Permission
+    # 'all' is just an alias for ('get', 'fetch', 'count', 'exists')
+    'auth.permission': {'ops': 'all', 'timeout': 60*60},
+
+    'metrics.*': {'ops': 'all', 'timeout': 5*60},
+    'metrics_users.*': {'ops': 'all', 'timeout': 5*60},
+
+    # And since ops is empty by default you can rewrite last line as:
+    '*.*': {'timeout': 60*60},
+}
+CACHEOPS_LRU = True
+CACHEOPS_DEGRADE_ON_FAILURE = True
