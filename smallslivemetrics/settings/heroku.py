76ef67b0a1ae99a6cb13bd9f598a5a458524ffd8
Filip Jukić <filip@jukic.me>
2015-10-12 10:58:38 +0200

    add sentry

--- smallslivemetrics/settings/heroku.py
+++ smallslivemetrics/settings/heroku.py
@@ -13,6 +13,9 @@ def env_var(key, default=None):
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "herokudefault")
 
+INSTALLED_APPS += (
+    'raven.contrib.django.raven_compat',
+)
 
 DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['CONN_MAX_AGE'] = 60
