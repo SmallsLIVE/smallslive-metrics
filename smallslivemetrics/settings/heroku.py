76bddb5bc08446640569b29770ef4afd65bf76bd
Filip Jukić <filip@jukic.me>
2015-10-12 10:52:02 +0200

    persistent db connections

--- smallslivemetrics/settings/heroku.py
+++ smallslivemetrics/settings/heroku.py
@@ -15,6 +15,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "herokudefault")
 
 
 DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['CONN_MAX_AGE'] = 60
 DATABASES['auth_db'] = dj_database_url.config('AUTH_DB_URL')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
